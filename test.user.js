395d8bdc9c8ada9f4855ec659c6b303dc404acb5
Jan Biniok <jan.biniok@raritan.com>
2021-01-22 12:52:15 +0100

    Update test.user.js

--- test.user.js
+++ test.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        http://*/*
+// @match        https://*/
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
